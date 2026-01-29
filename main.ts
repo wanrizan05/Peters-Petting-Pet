2049935183804975c4dbcc39f88ba4f43d26af6e
wanrizan05 <wanrizan@gmail.com>
2026-01-29 18:06:51 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,204 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    music.play(music.stringPlayable("E C E C E C E C ", 100), music.PlaybackMode.UntilDone)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+})
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    basic.showLeds(`
+        . . . . .
+        . . # . #
+        . . . . .
+        # # # . .
+        . # . . .
+        `)
+    music.play(music.stringPlayable("- A A B A B C5 B ", 100), music.PlaybackMode.UntilDone)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        # # . # #
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+    for (let index = 0; index < 5; index++) {
+        music.play(music.createSoundExpression(WaveShape.Noise, 1938, 1938, 0, 18, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    }
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showLeds(`
+        . . . . .
+        # . # . .
+        . . . . .
+        . . # # #
+        . . . # .
+        `)
+    music.play(music.stringPlayable("B C5 B A B A A - ", 100), music.PlaybackMode.UntilDone)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+})
+input.onPinPressed(TouchPin.P2, function () {
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    music.play(music.stringPlayable("- A A G A B B A ", 100), music.PlaybackMode.UntilDone)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+})
+input.onSound(DetectedSound.Loud, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # # # # #
+        . # # # .
+        `)
+    music.play(music.stringPlayable("C D E F G A B C5 ", 100), music.PlaybackMode.UntilDone)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        . # # # .
+        `)
+    for (let index = 0; index < 5; index++) {
+        music.play(music.createSoundExpression(WaveShape.Square, 1664, 2670, 0, 13, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    }
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    music.play(music.stringPlayable("E A D G E A D G ", 100), music.PlaybackMode.UntilDone)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    music.play(music.stringPlayable("- B B F B C5 C5 B ", 100), music.PlaybackMode.UntilDone)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+})
+input.onSound(DetectedSound.Quiet, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # # # # #
+        `)
+    music.play(music.stringPlayable("C5 B A G F E D C ", 100), music.PlaybackMode.UntilDone)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    music.play(music.stringPlayable("A C5 A C5 A C5 A C5 ", 100), music.PlaybackMode.UntilDone)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+})
+for (let index = 0; index < 5; index++) {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.InBackground)
+}
